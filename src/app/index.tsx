de65ef98b153dff916eb903fc7fff0d154601b17
zhouxingchi12345 <212292877+MSRformi@users.noreply.github.com>
2025-08-05 08:20:24 +0900

    Markup Header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,10 +5,10 @@ const Index = (): JSX.Element => {
   return (
     <View style={styles.container}>
       {/* 标题 */}
-      <View>
-        <View>
-          <Text>Memo App</Text>
-          <Text>Log out</Text>
+      <View style={styles.header}>
+        <View style={styles.headerInner}>
+          <Text style={styles.headerTitle}>Memo App</Text>
+          <Text style={styles.headerLogout}>Log out</Text>
         </View>
       </View>
       {/* 清单 */}
@@ -81,8 +81,32 @@ const Index = (): JSX.Element => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    //↑用于扩张
+
+    // justifyContent: "center",
+    // alignItems: "center",
+    backgroundColor: "#ffffff",
+  },
+  header: {
+    backgroundColor: "#467FD3",
+    height: 104,
+    justifyContent: "flex-end",
+  },
+  headerInner: {
     alignItems: "center",
+  },
+  headerLogout: {
+    position: "absolute",
+    right: 16,
+    bottom: 16,
+    color:'rgba(255,255,255,0.7)'
+  },
+  headerTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    fontWeight: "bold",
+    color:'#ffffff'
   },
 });
 

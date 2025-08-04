a1317a6cd4144a44a2f0beba7a087e6ad2258a7b
zhouxingchi12345 <212292877+MSRformi@users.noreply.github.com>
2025-08-05 08:05:08 +0900

    Markup MemoList screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,89 @@
 import { JSX } from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 const Index = (): JSX.Element => {
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      {/* 标题 */}
+      <View>
+        <View>
+          <Text>Memo App</Text>
+          <Text>Log out</Text>
+        </View>
+      </View>
+      {/* 清单 */}
+      <View>
+        {/* MemoItem */}
+        <View>
+          <View>
+            <Text>购物清单</Text>
+            <Text>2025年8月5日</Text>
+          </View>
+
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+        {/* MemoItem */}
+        <View>
+          <View>
+            <Text>购物清单</Text>
+            <Text>2025年8月5日</Text>
+          </View>
+
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+        {/* MemoItem */}
+        <View>
+          <View>
+            <Text>购物清单</Text>
+            <Text>2025年8月5日</Text>
+          </View>
+
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+        {/* MemoItem */}
+        <View>
+          <View>
+            <Text>购物清单</Text>
+            <Text>2025年8月5日</Text>
+          </View>
+
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+        {/* MemoItem */}
+        <View>
+          <View>
+            <Text>购物清单</Text>
+            <Text>2025年8月5日</Text>
+          </View>
+
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+      </View>
+
+      {/* 加号 */}
+      <View>
+        <Text>+</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default Index;

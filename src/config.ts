428be23d72f81a65b5df634e2c5791866233389c
zhouxingchi12345 <212292877+MSRformi@users.noreply.github.com>
2025-08-13 09:06:10 +0900

    .env文件相关的设置

--- src/config.ts
+++ src/config.ts
@@ -5,20 +5,19 @@ import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 import { getReactNativePersistence } from "firebase/auth/react-native";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB47xWUb1E9UdjagJrmoAgV0veUr867BiY",
-  authDomain: "memoapp-7eb4a.firebaseapp.com",
-  projectId: "memoapp-7eb4a",
-  storageBucket: "memoapp-7eb4a.firebasestorage.app",
-  messagingSenderId: "838714249897",
-  appId: "1:838714249897:web:4e5273d43712a719f2cfb8",
+  apiKey: process.env.EXPO_PUBLIC_FB_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FB_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FB_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FB_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FB_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-
 const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
 });
-
 const db = getFirestore(app);
 
 export { app, auth, db };
+

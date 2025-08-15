acc1eff7d4d78c57e81e5414460f450534f1480c
zhouxingchi12345 <212292877+MSRformi@users.noreply.github.com>
2025-08-16 06:49:03 +0900

    登出按钮功能的实现

--- src/components/LogOut.tsx
+++ src/components/LogOut.tsx
@@ -1,9 +1,21 @@
 import { JSX } from "react";
-import { TouchableOpacity, Text, StyleSheet } from "react-native";
+import { TouchableOpacity, Text, StyleSheet, Alert } from "react-native";
+import { signOut } from "firebase/auth";
+import { router } from "expo-router";
+import { auth } from "../config";
 
+const handlePress = (): void => {
+  signOut(auth)
+    .then(() => {
+      router.replace("/auth/log_in");
+    })
+    .catch(() => {
+      Alert.alert("登出失败");
+    });
+};
 const LogOutButton = (): JSX.Element => {
   return (
-    <TouchableOpacity>
+    <TouchableOpacity onPress={handlePress}>
       <Text style={styles.text}>登出</Text>
     </TouchableOpacity>
   );
@@ -13,7 +25,7 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 12,
     lineHeight: 24,
-    color: "rgba(255,255,255,0.7)"
+    color: "rgba(255,255,255,0.7)",
   },
 });
 
